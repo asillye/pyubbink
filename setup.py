429037f0350299c6372abbc3fabf462f19cebe1a
Adam Sillye <adam@berriesand.co>
2021-05-29 19:20:14 +0200

    Packaging

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-setuptools.setup(
-     name='pyubbink',  
-     version='0.1',
-     scripts=['pyubbink'] ,
-     author="Adam Sillye",
-     author_email="adam@berriesand.co",
-     description="An unofficial Ubbink Ubiflux Vigor modbus package",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/asillye/pyubbink",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
+setup(
+    name='pyubbink',  
+    version='0.1',
+    scripts=['pyubbink'] ,
+    author="Adam Sillye",
+    author_email="adam@berriesand.co",
+    description="'Python interface for Ubbink Ubiflux Vigor ventillation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/asillye/pyubbink",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=list(val.strip() for val in open('requirements.txt')),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
      ],
  )
